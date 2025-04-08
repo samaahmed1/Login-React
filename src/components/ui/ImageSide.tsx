0f06dfaaf33dd8d9eda29ac41f8d4b2f5f49ca78
samaahmed1 <sama34777@gmail.com>
2025-04-08 11:11:30 +0200

    Add: ImageSide Component

---
+++ src/components/ui/ImageSide.tsx
@@ -0,0 +1,14 @@
+export default function ImageSide() {
+  return (
+    <>
+      <section>
+        <div className="relative">
+          <img className="relative" src="/public/images/Image.webp" alt="image" />
+          <p className="text-white text-sm absolute bottom-14 left-14 text-center">
+            “Two lunar months revealed Earth's fragile beauty against vast <br /> silence, transforming my view of our place in the universe.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
