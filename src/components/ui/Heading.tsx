0ad5736baa068545fe175d2a7ad8257015fdef4a
samaahmed1 <sama34777@gmail.com>
2025-04-08 10:58:31 +0200

    Add: Heading Component

---
+++ src/components/ui/Heading.tsx
@@ -0,0 +1,12 @@
+export default function Heading() {
+  return (
+    <>
+      <section className="flex justify-center flex-col gap-2">
+        <h1 className="text-4xl md:text-6xl m-auto text-center font-bold tracking-tight bg-gradient-to-r from-[#FFFFFF] via-[#763AF5] to-[#A604F2] bg-clip-text text-transparent inline-block">
+          Get in touch
+        </h1>
+        <p className="text-white text-lg md:text-xl text-center tracking-tight">Reach out, and let's create a universe of possibilities together!</p>
+      </section>
+    </>
+  );
+}
